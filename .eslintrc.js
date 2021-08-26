5ed3e5e4da2ed95eb7a3633903d9e88053826221
Henry Hobhouse <henry@tray.io>
2021-08-26 21:03:53 +0100

    chore: further optimise linting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,63 +1,107 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
+  env: {
+    browser: true,
+    node: true,
   },
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
     'esnext',
+    'plugin:promise/recommended',
+    'plugin:unicorn/recommended',
     'prettier',
     'plugin:prettier/recommended',
   ],
-  settings: {
-    react: {
-      version: 'detect',
-    },
-    'import/resolver': {
-      typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
-    },
-  },
-  env: {
-    browser: true,
-    node: true,
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    'no-use-before-define': 0,
-    'import/no-nodejs-modules': 0,
-    'import/prefer-default-export': 0,
-    '@typescript-eslint/explicit-module-boundary-types': 0,
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        vars: 'all',
-        args: 'after-used',
-        ignoreRestSiblings: true,
-      },
-    ],
-    '@typescript-eslint/explicit-function-return-type': 0,
-    'import/order': [
-      'error',
-      {
-        'newlines-between': 'always',
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-      },
-    ],
-    'no-console': 'error',
-  },
   overrides: [
     {
       files: ['*.js'],
       rules: {
         'import/no-commonjs': 0,
+        'unicorn/prefer-module': 0,
       },
     },
   ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'json-format', 'sort-keys-fix'],
+  rules: {
+    '@typescript-eslint/array-type': [
+      'warn',
+      {
+        default: 'generic',
+      },
+    ],
+    '@typescript-eslint/consistent-type-assertions': [
+      'error',
+      {
+        assertionStyle: 'as',
+        objectLiteralTypeAssertions: 'allow',
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/method-signature-style': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'after-used',
+        ignoreRestSiblings: true,
+        vars: 'all',
+      },
+    ],
+    'import/newline-after-import': ['error', { count: 1 }],
+    'import/no-nodejs-modules': 0,
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          caseInsensitive: true,
+          order: 'asc',
+        },
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index', 'object'],
+        'newlines-between': 'always',
+      },
+    ],
+    'import/prefer-default-export': 0,
+    'no-console': 'error',
+    'no-use-before-define': 0,
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', next: 'return', prev: '*' },
+    ],
+    'sort-keys-fix/sort-keys-fix': [
+      'error',
+      'asc',
+      { caseSensitive: true, natural: false },
+    ],
+    'unicorn/no-array-reduce': [
+      'error',
+      {
+        allowSimpleOperations: true,
+      },
+    ],
+    'unicorn/prefer-node-protocol': 0,
+    'unicorn/prevent-abbreviations': [
+      'error',
+      {
+        replacements: {
+          props: false,
+        },
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
+    },
+    react: {
+      version: 'detect',
+    },
+  },
 };
