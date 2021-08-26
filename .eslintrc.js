76a20d277d8df47bf5b4b688ed4d8f87c7d992cf
Henry Hobhouse <henry@tray.io>
2021-08-26 06:19:59 +0100

    chore: add linting

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'esnext',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-use-before-define': 0,
+    'import/no-nodejs-modules': 0,
+    'import/prefer-default-export': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 0,
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'no-console': 'error',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'all',
+  singleQuote: true,
+  tabWidth: 2,
+  proseWrap: 'always',
+  semi: true,
+  bracketSpacing: true,
+  useTabs: false,
+};
