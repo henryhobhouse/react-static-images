663846aea0252b9a3a41f314d15fbe8926522f74
Henry Hobhouse <henry@tray.io>
2021-09-07 18:42:56 +0100

    feat(process-static-images): retrieve images meta

--- src/process-static-images/process-static-images.test.ts
+++ src/process-static-images/process-static-images.test.ts
@@ -1,5 +1,48 @@
+const mockInfoLogger = jest.fn();
+const mockGetImageMetaData = jest
+  .fn()
+  .mockImplementation(() => ({ imageFilesMetaData: [] }));
+
+import { processStaticImages } from './process-static-images';
+
+jest.mock('../logger', () => ({
+  logger: {
+    info: mockInfoLogger,
+  },
+}));
+
+jest.mock('./get-images-meta-data', () => ({
+  getImageMetaData: mockGetImageMetaData,
+}));
+
 describe('processStaticImages', () => {
-  it.skip('does something', () => {
-    return;
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('will request to get all images meta data with no arguements', async () => {
+    await processStaticImages();
+    expect(mockGetImageMetaData).toBeCalledWith();
+  });
+
+  it('will log if no images found and exit from the process', async () => {
+    mockGetImageMetaData.mockImplementationOnce(() => ({
+      imageFilesMetaData: [],
+    }));
+    await processStaticImages();
+    expect(mockInfoLogger.mock.calls.pop()[0]).toBe(
+      'No new images to process.',
+    );
+  });
+
+  it('will log out how many images need processing when images are found', async () => {
+    const mockImageMetaDatas = [{}, {}];
+    mockGetImageMetaData.mockImplementationOnce(() => ({
+      imageFilesMetaData: mockImageMetaDatas,
+    }));
+    await processStaticImages();
+    expect(mockInfoLogger.mock.calls[1][0]).toBe(
+      `${mockImageMetaDatas.length} total unprocessed images`,
+    );
   });
 });
--- src/process-static-images/process-static-images.ts
+++ src/process-static-images/process-static-images.ts
@@ -5,8 +5,19 @@ import { getImageMetaData } from './get-images-meta-data';
 export const processStaticImages = async () => {
   logger.info('Processing static images');
 
-  const { imagesMetaData } = await getImageMetaData();
-  // get meta data on images that need processing paths, total
+  const { imageFilesMetaData } = await getImageMetaData();
+
+  const totalImagesToProcess = imageFilesMetaData.length;
+
+  // if not accepted image types found then exit
+  if (totalImagesToProcess === 0) {
+    logger.info('No new images to process.');
+
+    return;
+  }
+
+  logger.info(`${totalImagesToProcess} total unprocessed images`);
+
   // create thumbnails and required image sizes
   // caching - ensure that we don't process images that we already
   // browser caching - adding hash fingerprint to file name
