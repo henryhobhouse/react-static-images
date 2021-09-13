9dd1bffcd2cb0ef5e97fb92dba81735a2a39c0ab
Henry Hobhouse <henry@tray.io>
2021-09-13 14:08:21 +0100

    refactor(cli): use build dir as fallback for tests

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
 
 require = require('esm')(module /*, options*/);
-require('../cli').cli(process.argv);
+
+try {
+  require('../cli').cli(process.argv);
+} catch {
+  // if using the library as a symlink or any way that isn't using it after a CI build
+  // then to try to looking for the CLI module in the build directory instead
+  require('../dist/cli').cli(process.argv);
+}
